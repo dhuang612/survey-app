8e14f3d91a075d279f3aabc72f5bacb28fa9181a
dhuang612 <dhuang@madisoncollege.edu>
2019-05-06 14:31:16 -0400

    added keys.js to .gitignore

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-//route handler
-app.get('/', (req, res) => {
-  res.send({ cya: 'later!' });
-});
+//creates a new instance of google auth
+passport.use(
+  new GoogleStrategy({
+    clientID: Google_Client_ID,
+    clientSecret: Google_client_secret,
+    callbackURL: 'http://example.com/auth/google/callback'
+  })
+);
+/*
+you use google developers console to gen the application
+you then pass in the client ID
+use 
+http://localhost:5000/auth/google/callback
+
+clientID: 596213864696-87iuiau2g2psnntmuppbu7kk8em03lq8.apps.googleusercontent.com
+client secret: AiPzJmNHZZwYqGBm8RRjgKQF
+*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
