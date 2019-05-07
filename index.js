1589d9c17b2c6933577b174ead25770fc9eb7851
dhuang612 <dhuang@madisoncollege.edu>
2019-05-07 12:35:40 -0400

    added nodemon to not have to restart server

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 //creates a new instance of google auth
 passport.use(
-  new GoogleStrategy({
-    clientID: Google_Client_ID,
-    clientSecret: Google_client_secret,
-    callbackURL: 'http://example.com/auth/google/callback'
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token: ', accessToken);
+      console.log('profile: ', profile);
+      console.log('refresh token: ', refreshToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 );
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 /*
 you use google developers console to gen the application
 you then pass in the client ID
