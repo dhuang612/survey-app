ae36d40d689ac568e5ce065ab70cfe674445f526
dhuang612 <49162249+dhuang612@users.noreply.github.com>
2019-05-11 18:42:14 -0400

    Add files via upload
    committed info into db and displayed on webpage.

---
+++ authRoutes.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+//cookie based authentication comes with a header that has a code that will be included with follow-up requests
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
+};
+
+/*
+
+*/
--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+mongoose.connect(keys.mongoURI);
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+//user's schema that we need to require mongoose order of operations matter
+require('./models/User');
+require('./services/passport');
+//this can be left as a require statement because it doesn't need to be passed anything.
+require('./routes/authRoutes')(app);
+/*
+you use google developers console to gen the application
+you then pass in the client ID
+use 
+http://localhost:5000/auth/google/callback
 
-//route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there!' });
-});
+clientID: 596213864696-87iuiau2g2psnntmuppbu7kk8em03lq8.apps.googleusercontent.com
+client secret: AiPzJmNHZZwYqGBm8RRjgKQF
+*/
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
