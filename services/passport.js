09b7c19e2cfb88a7123c39be8425c0f78615ea15
dhuang612 <dhuang@madisoncollege.edu>
2019-05-11 21:02:42 -0400

    resolved routes and displayed google id on webpage

--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: 'http://localhost:5000/auth/google/callback'
+      //auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(exisitingUser => {
