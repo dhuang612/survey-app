c7a7f4201096248b9406eb356bc0506afa75cf3e
dhuang612 <49162249+dhuang612@users.noreply.github.com>
2019-05-27 16:33:51 -0400

    Add files via upload
    resolved error related to internal heroku server error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 mongoose.connect(keys.mongoURI);
+
 const app = express();
 app.use(
   cookieSession({
