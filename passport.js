4f2a574f3c2fe4db5b2ef11b11735d21df44015c
dhuang612 <49162249+dhuang612@users.noreply.github.com>
2019-05-30 13:01:15 -0400

    Add files via upload
    refactored promises to async await

--- passport.js
+++ passport.js
@@ -10,6 +10,8 @@ passport.serializeUser((user, done) => {
   /*  
   After they are in our mongoDB we no longer care about the google id
   because each instance inside our db will have a mongoDB id!
+  dig more into the npm google oauth20 package and figure out what is going on with internal
+  server error
   */
   done(null, user.id);
 });
@@ -19,33 +21,57 @@ passport.deserializeUser((id, done) => {
     done(null, user);
   });
 });
+/*
+look into confirming that proxy is working correctly.
+https://github.com/jaredhanson/passport-oauth2/issues/59
+
+*/
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'http://localhost:5000/auth/google/callback'
-      //auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(exisitingUser => {
-        if (exisitingUser) {
-          //we already have a record for the user use the done function and pass 2 params
-          done(null, exisitingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const exisitingUser = await User.findOne({ googleId: profile.id });
+
+      if (exisitingUser) {
+        //we already have a record for the user use the done function and pass 2 params
+        return done(null, exisitingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
-
 /*
 Because we aren't returning anything in here we can pass the require 
 statement as the function
+
+passport google strategy code
+
+  User.findOne({ googleId: profile.id })
+        .then(exisitingUser => {
+          if (exisitingUser) {
+            //we already have a record for the user use the done function and pass 2 params
+            done(null, exisitingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+
+
+
 
 
 */
