6ece0dbff96acd96cad2944bbbaf142adc252b9c
dhuang612 <dhuang@madisoncollege.edu>
2019-05-31 12:48:44 -0400

    continuing work on the react side of applicatio

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//dummy routes for testing.
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
+
+/*
+for BrowserRouter you use exact on the lowest common denominator 
+
+*/
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+//stand in dummy so that redux doesnt crash with an error
+const store = createStore(reducers, {}, applyMiddleware());
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.querySelector('#root')
+);
+
+/*
+
+we render store and provider here
+provider = bonding layer between react and redux
+*/
