18f90c9e6eaab91a851cbc6dd2e9ac77998448a3
dhuang612 <49162249+dhuang612@users.noreply.github.com>
2019-06-02 14:44:40 -0400

    Add files via upload
    created new route to handle signing in the user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,27 +8,22 @@ module.exports = app => {
     })
   );
   //if there are errors related to logging in capture them.
-  app.get('/auth/google/callback', function(req, res, next) {
-    passport.authenticate('google', function(err, user, info) {
-      if (err) {
-        return console.log(next(err));
-      }
-      if (!user) {
-        return res.redirect('/');
-      }
-      req.logIn(user, function(err) {
-        if (err) {
-          return console.log(next(err));
-        }
-        return res.redirect('/');
-      });
-    })(req, res, next);
-  });
+  app.get(
+    '/auth/google/callback',
+    //middleware
+    passport.authenticate('google'),
+    //after we successfully log in we add in a redirect to get to the right place
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
     res.send(req.user);
   });
+
+  //this will allow us to confirm if the user is signed in
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
