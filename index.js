6c1b2c3829a0f0b1c5473786fbb25e71eca83179
dhuang612 <dhuang@madisoncollege.edu>
2019-06-06 14:44:10 -0400

    added missing common

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
   //if express doesn't recognize the route here is the catchall location for anything else
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname  'client', 'build' ,'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build' ,'index.html'));
   });
 }
 
