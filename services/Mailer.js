f8f2e44726ef87b2bed415f051982b746235ab77
dhuang612 <dhuang@madisoncollege.edu>
2019-06-09 17:00:30 -0400

    working on backend of sending our survey emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,6 +8,7 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     //es2015 syntax
     super();
+    this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
@@ -16,6 +17,7 @@ class Mailer extends helper.Mail {
     //built in function to add the body of content to the email
     this.addContent(this.body);
     this.addClickTracking();
+    this.addRecipients();
   }
   //helper functions
   formatAddresses(recipients) {
@@ -29,6 +31,22 @@ class Mailer extends helper.Mail {
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
+  }
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+    const response = this.sgApi.API(request);
+    return response;
   }
 }
 module.exports = Mailer;
