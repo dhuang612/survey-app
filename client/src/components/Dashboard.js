be6e2cad17bb8b1437b2afc96ac39c3ee82f61fe
dhuang612 <dhuang@madisoncollege.edu>
2019-06-11 11:45:49 -0400

    creating front end link to creating new surveys

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const DashBoard = () => {
+  return (
+    <div>DashBoard</div>,
+    (
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red">
+          <i className="large material-icons">add</i>
+        </a>
+      </div>
+    )
+  );
+};
+export default DashBoard;
