b7013debe3ee0d999226b1094bfedbec4322f09c
dhuang612 <dhuang@madisoncollege.edu>
2019-06-11 17:27:12 -0400

    working create survey template

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,16 @@
+/*SurveyField contains logic and 
+we are separating this into its own component to customize labels and input fields with
+props
+we aren't making a full component atm
+redux form allows us to wiring up the event handlers for changes to this input
+*/
+import React from 'react';
+//we can access props even if we don't create a component
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+};
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,37 @@
 //shows the new survey
 import React, { Component } from 'react';
+import _ from 'lodash';
 //very similar setup to redux connect helper & allows us to talk to redux store!
 import { reduxForm, Field } from 'redux-form';
 //onSubmit below is passed to use directly from redux form we then call it and set it to submit
+import SurveyField from './SurveyField';
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 class SurveyForm extends Component {
+  //helper method to render labels
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          component={SurveyField}
+          type="text"
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="SurveyTitle" component="input" />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
