bab7e8fe964534391dd5fdc7657b04dc34000c55
dhuang612 <dhuang@madisoncollege.edu>
2019-06-11 12:12:41 -0400

    replaced html anchor tag with react router dom link

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,17 @@
-import React from 'react';
-const DashBoard = () => {
-  return (
-    <div>DashBoard</div>,
-    (
-      <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red">
-          <i className="large material-icons">add</i>
-        </a>
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+class DashBoard extends Component {
+  render() {
+    return (
+      <div>
+        DashBoard
+        <div className="fixed-action-btn">
+          <Link to="/surveys/new" className="btn-floating btn-large red">
+            <i className="large material-icons">add</i>
+          </Link>
+        </div>
       </div>
-    )
-  );
-};
+    );
+  }
+}
 export default DashBoard;
