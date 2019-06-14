4d9dd1fc9c9ee1b04f57fc778e6c7afff7a13f5e
dhuang612 <dhuang@madisoncollege.edu>
2019-06-14 15:14:44 -0400

    allowing validation to accept trailing commas

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,10 +1,14 @@
 const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
 export default emails => {
   const invalidEmails = emails
     .split(',')
     .map(email => email.trim())
+    .filter(email => email.replace(/,\s*$/, ''))
     .filter(email => !re.test(email));
-  if (invalidEmails.length && !invalidEmails === ',') {
+  //removes ending commas
+
+  if (invalidEmails.length) {
     return `These emails are invalid: ${invalidEmails}`;
   }
   return;
