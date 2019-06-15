45aae653026425cb39dd64df9d1b3761127b08e4
dhuang612 <dhuang@madisoncollege.edu>
2019-06-14 20:50:08 -0400

    working on showing correct component on front-end

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -2,14 +2,22 @@ const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?
 
 export default emails => {
   const invalidEmails = emails
+    //removes ending commas
     .split(',')
     .map(email => email.trim())
     .filter(email => email.replace(/,\s*$/, ''))
     .filter(email => !re.test(email));
-  //removes ending commas
 
   if (invalidEmails.length) {
     return `These emails are invalid: ${invalidEmails}`;
   }
   return;
 };
+/*
+
+original
+/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+
+
+(([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)(\s*(;|,)\s*|\s*$)
+*/
