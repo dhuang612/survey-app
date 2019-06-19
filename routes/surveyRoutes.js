372563715f6e3d19fab09192f37d9fa4980397d3
dhuang612 <dhuang@madisoncollege.edu>
2019-06-19 11:44:23 -0400

    parsing data received from api

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+//comes with node js
+const { URL } = require('url');
 const mongoose = require('mongoose');
 //include in the survey schema
 const Survey = mongoose.model('surveys');
@@ -12,8 +16,12 @@ module.exports = app => {
     res.send('Thanks for the feedback!');
   });
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, event => {
+      const pathname = new URL(event.url).pathname;
+      //survey id and path new parser
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      console.log(p.test(pathname));
+    });
   });
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     //we are using body parser
