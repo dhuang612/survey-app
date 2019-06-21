d4aef9dbb1181517e73d8286cb77e0a237d9c630
dhuang612 <dhuang@madisoncollege.edu>
2019-06-21 17:14:06 -0400

    passed data from backend to front end confirmed via console.log

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 class DashBoard extends Component {
   //in React we use Link tags instead of anchor
   render() {
     return (
       <div>
-        DashBoard
+        <SurveyList />
         <div className="fixed-action-btn">
           <Link to="/surveys/new" className="btn-floating btn-large red">
             <i className="large material-icons">add</i>
